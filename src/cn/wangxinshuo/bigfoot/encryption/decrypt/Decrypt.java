9eb737ef4659de7d0d12489db86788acea2dcb48
XinShuoWang <xinshuowang@163.com>
2019-11-26 21:35:08 +0800

    更新可执行文件

--- src/cn/wangxinshuo/bigfoot/encryption/decrypt/Decrypt.java
+++ src/cn/wangxinshuo/bigfoot/encryption/decrypt/Decrypt.java
@@ -6,7 +6,11 @@ public class Decrypt {
     public static byte[] decrypt(byte[] data, int len, Configuration configuration){
         byte[] bytes = new byte[len];
         for (int i = 0; i < len; i++) {
-            bytes[i] = (byte) ((byte) (configuration.getPassword()) ^ data[i]);
+            if(data[i] == -128){
+                bytes[i] = data[i];
+            }else {
+                bytes[i] = (byte) -data[i];
+            }
         }
         //System.arraycopy(data,0,bytes,0,len);
         return bytes;
