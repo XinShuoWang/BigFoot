e18c30ee42876059a47fc5f934d44bbbe6072daa
wangxinshuo <18862616362@163.com>
2019-11-26 19:46:38 +0800

    删除测试透传代码，增加加密代码

--- src/cn/wangxinshuo/bigfoot/encryption/decrypt/Decrypt.java
+++ src/cn/wangxinshuo/bigfoot/encryption/decrypt/Decrypt.java
@@ -5,10 +5,10 @@ import cn.wangxinshuo.bigfoot.conf.Configuration;
 public class Decrypt {
     public static byte[] decrypt(byte[] data, int len, Configuration configuration){
         byte[] bytes = new byte[len];
-//        for (int i = 0; i < len; i++) {
-//            bytes[i] = (byte) ((byte) (configuration.getPassword()) ^ data[i]);
-//        }
-        System.arraycopy(data,0,bytes,0,len);
+        for (int i = 0; i < len; i++) {
+            bytes[i] = (byte) ((byte) (configuration.getPassword()) ^ data[i]);
+        }
+        //System.arraycopy(data,0,bytes,0,len);
         return bytes;
     }
 }
--- src/cn/wangxinshuo/bigfoot/encryption/encrypt/Encrypt.java
+++ src/cn/wangxinshuo/bigfoot/encryption/encrypt/Encrypt.java
@@ -5,10 +5,10 @@ import cn.wangxinshuo.bigfoot.conf.Configuration;
 public class Encrypt {
     public static byte[] encrypt(byte[] data, int len, Configuration configuration){
         byte[] bytes = new byte[len];
-//        for (int i = 0; i < len; i++) {
-//            bytes[i] = (byte) ((byte) (configuration.getPassword()) ^ data[i]);
-//        }
-        System.arraycopy(data,0,bytes,0,len);
+        for (int i = 0; i < len; i++) {
+            bytes[i] = (byte) ((byte) (configuration.getPassword()) ^ data[i]);
+        }
+        //System.arraycopy(data,0,bytes,0,len);
         return bytes;
     }
 }
