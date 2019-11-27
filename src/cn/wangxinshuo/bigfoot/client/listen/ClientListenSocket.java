a92a8650309fe88f9924b364f7eeda01809f5d22
wangxinshuo <18862616362@163.com>
2019-11-27 20:39:14 +0800

    优化错误捕获代码

--- src/cn/wangxinshuo/bigfoot/client/listen/ClientListenSocket.java
+++ src/cn/wangxinshuo/bigfoot/client/listen/ClientListenSocket.java
@@ -21,17 +21,21 @@ public class ClientListenSocket {
         serverSocket = new ServerSocket(configuration.getListenPort());
     }
 
-    public void listen() throws IOException {
+    public void listen() {
         while (true) {
-            // 未连通前线程阻塞，连通后开启一个socket通道线程后继续监听端口
-            Socket clientSocket = this.serverSocket.accept();
-            SSLSocket serverSocket = SSLSocketBigFoot
-                    .getClientSocket(configuration.getDestinationAddress(),
-                            configuration.getDestinationPort());
-            // 先准备好接收程序
-            new ClientReverseSocketReceive(clientSocket, serverSocket, configuration).start();
-            // 开启转发请求程序
-            new ClientReverseSocketSend(clientSocket, serverSocket, configuration).start();
+            try {
+                // 未连通前线程阻塞，连通后开启一个socket通道线程后继续监听端口
+                Socket clientSocket = this.serverSocket.accept();
+                SSLSocket serverSocket = SSLSocketBigFoot
+                        .getClientSocket(configuration.getDestinationAddress(),
+                                configuration.getDestinationPort());
+                // 先准备好接收程序
+                new ClientReverseSocketReceive(clientSocket, serverSocket, configuration).start();
+                // 开启转发请求程序
+                new ClientReverseSocketSend(clientSocket, serverSocket, configuration).start();
+            }catch (IOException ignore){
+
+            }
         }
     }
 }
